4339d46b440df01d8a5ace5562f5de87566e818b
Jackie <jacquelinederigs@gmail.com>
2025-04-10 21:00:30 +0200

    installed framework7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { setupIonicReact } from '@ionic/react';
 import './App.css'
+
+setupIonicReact();
 
 function App() {
   const [count, setCount] = useState(0)
