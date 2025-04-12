15b35ed0fbfa15a9822558810c97adc2ee2f8844
Jackie <jacquelinederigs@gmail.com>
2025-04-12 11:54:44 +0200

    basic bottom nav

---
+++ src/components/general/BottomNav.jsx
@@ -0,0 +1,51 @@
+import {
+	House,
+	Notebook,
+	Plus,
+	CookingPot,
+	CircleUserRound,
+} from "lucide-react";
+import { NavLink, useLocation } from "react-router-dom";
+
+/*<meta name="viewport" content="viewport-fit=cover">is required for responsivness of the dock in iOS.*/
+
+const BottomNav = () => {
+	const location = useLocation();
+	const path = location.pathname;
+
+	return (
+		<div className="dock dock-sm">
+			<button className={path === "/" ? "dock-active" : ""}>
+				<NavLink to="/">
+					<House />
+				</NavLink>
+			</button>
+
+			<button className={path === "/diary" ? "dock-active" : ""}>
+				<NavLink to="/diary">
+					<Notebook />
+				</NavLink>
+			</button>
+
+			<button className={path === "/search" ? "dock-active" : ""}>
+				<NavLink to="/search">
+					<Plus />
+				</NavLink>
+			</button>
+
+			<button className={path === "/recipes" ? "dock-active" : ""}>
+				<NavLink to="/recipes">
+					<CookingPot />
+				</NavLink>
+			</button>
+
+			<button className={path === "/account" ? "dock-active" : ""}>
+				<NavLink to="/account">
+					<CircleUserRound />
+				</NavLink>
+			</button>
+		</div>
+	);
+};
+
+export default BottomNav;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import BottomNav from "../components/general/BottomNav";
 
 const MainLayout = () => {
 	return (
 		<>
 			<div className="container mx-auto">
 				<Outlet />
+                <BottomNav />
 			</div>
 		</>
 	);
