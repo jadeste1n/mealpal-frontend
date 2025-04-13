fd8425818fbdd6a114aae614727d6a961ea6f85b
Lena Hauer <lena.hauer@me.com>
2025-04-13 19:26:20 +0200

    Added signup and login functionality

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
